b0aa53b94e1e3df2a654e40b57946b4d33233c7f
lhw <1170137052@qq.com>
2018-06-12 11:49:07 +0800

    项目初始化

---
+++ huawei.spider.center.jar/src/main/java/com/huawei/spider/center/jar/Provider.java
@@ -0,0 +1,15 @@
+package com.huawei.spider.center.jar;
+
+/**
+ * 功能：Provider
+ * 描述：程序入口
+ * 作者：laihuawei(laihuawei@lianj.com)
+ * 日期：2018年06月2018/6/12日 10:51
+ * 版权所有：广东联结网络技术有限公司 版权所有(C)
+ */
+public class Provider {
+
+    public static void main(String[] args) {
+
+    }
+}
