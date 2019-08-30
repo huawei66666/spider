787d0faa74a5acfd162b35482b15914d17752d52
lhw <laihuawei@lianj.com>
2019-08-30 16:27:02 +0800

    爬虫优化

--- huawei.spider.center.jar/src/main/java/com/huawei/spider/center/parsers/BoBoParser.java
+++ huawei.spider.center.jar/src/main/java/com/huawei/spider/center/parsers/BoBoParser.java
@@ -1,30 +1,22 @@
 package com.huawei.spider.center.parsers;
 
-import com.huawei.spider.center.beans.UrlInfoBo;
+import com.huawei.spider.center.utils.FileUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created on 2019/8/29.
  */
 public class BoBoParser {
 
-    private String localFilePath = "D:/happy.txt";
-
-    public BoBoParser() {
-    }
-
-    public BoBoParser(String localFilePath) {
-        this.localFilePath = localFilePath;
-    }
+    public static String local = "/mydoc/m3u8/m3u8.txt";
 
     public static void main(String[] args) {
         parse();
@@ -35,8 +27,7 @@ public class BoBoParser {
      *
      * @throws Exception
      */
-    public static List<UrlInfoBo> parse() {
-        List<UrlInfoBo> result = new ArrayList<>();
+    public static void parse() {
         try {
             System.out.println("链接开始提取...");
 
@@ -44,117 +35,70 @@ public class BoBoParser {
             Document doc = Jsoup.connect(domain).get();
 //            System.out.println(doc);
             if (doc != null) {
+                String urls = "";
                 String scripts = doc.select("script").attr("src");
-                System.out.println(scripts);
+//                System.out.println("========= js地址: ======>>>" + scripts);
                 if (scripts.indexOf("fuck/ip") != -1) {
-                    URL u = new URL(scripts);
-                    InputStream ism = u.openStream();
-                    BufferedInputStream bf = new BufferedInputStream(ism);
-                    byte[] bytes = new byte[1024];
-                    int len = bf.read(bytes);
-                    StringBuffer sb = new StringBuffer();
-                    while (len != -1) {
-                        len = bf.read(bytes);
-                        String str = new String(bytes, "utf-8");
-                        sb.append(str);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(scripts).openStream()));
+                    String line, iframe = "";
+                    while (StringUtils.isNotBlank(line = reader.readLine())) {
+                        if (line.indexOf("frame1") != -1) {
+                            iframe = line.substring(line.indexOf("http"), line.indexOf(".com") + 4);
+                            if (StringUtils.isNotBlank(iframe)) {
+                                iframe = iframe.replaceAll("\\\\", "");
+//                                System.out.println("========= iframe: ======>>>" + iframe);
+                            }
+                            break;
+                        }
                     }
-                    String s = sb.toString();
-                    System.out.println(sb);
-
-
+                    if (StringUtils.isNotBlank(iframe)) {
+                        Document iframeDoc = Jsoup.connect(iframe).get();
+                        if (iframeDoc != null) {
+                            Elements videoUrlList = iframeDoc.select(".video-col a[href]");
+                            for (Element element : videoUrlList) {
+                                String videodetail = element.attr("href");
+                                if (StringUtils.isNotBlank(videodetail)) {
+//                                    System.out.println("===== 视频详情链接地址： ====>>>" + iframe + videodetail);
+                                    Document videoDoc = Jsoup.connect(iframe + videodetail).get();
+                                    if (videoDoc != null) {
+                                        Elements playera = videoDoc.select(".text-white");
+                                        if (playera != null) {
+                                            String playeraddr = iframe + playera.attr("href");
+                                            if (StringUtils.isNotBlank(playeraddr)) {
+//                                                System.out.println("===== 视频播放页面： ====>>>" + playeraddr);
+                                                Document playerDoc = Jsoup.connect(playeraddr).get();
+                                                Elements playerscripts = playerDoc.select("script");
+                                                if (playerscripts != null) {
+                                                    for (Element e : playerscripts) {
+                                                        if (e.toString().indexOf("player_data") != -1) {
+                                                            String m3u8 = e.toString();
+                                                            if (StringUtils.isNotBlank(m3u8) && m3u8.indexOf("https") != -1) {
+                                                                m3u8 = m3u8.substring(m3u8.indexOf("https"), m3u8.indexOf(".m3u8") + 5);
+                                                                m3u8 = m3u8.replaceAll("\\\\", "");
+                                                                urls += m3u8 + "\n";
+                                                                System.out.println("===== 已提取视频源地址： ====>>> " + m3u8);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
 
+                // 写入文件
+                if (StringUtils.isNotBlank(urls) && StringUtils.isNotBlank(local)) {
+                    FileUtil.writeToFile(local, urls);
+                }
             }
-
-//            Elements elements = doc.select(".caoporn_Maincontentfive .index_videoshow a");
-//            String info = "";
-//            for (Element a : elements) {
-//                String desc = a.select("p").html();
 //
-//                String src = a.select("img").attr("src");
-//                String img = domain + src;
-//
-//                String html = a.attr("href");
-//                String url = getDownloadUrl(domain + html);
-//                if (StringUtils.isNoneBlank(url)) {
-//                    info += desc + "  img: " + img + "   downloadUrl: " + url + "\n";
-//                    System.out.println(info);
-//
-//                    UrlInfoBo urlInfoBo = new UrlInfoBo();
-//                    urlInfoBo.setUrl(url);
-//                    String name = url.substring(url.lastIndexOf("/") + 1);
-//                    urlInfoBo.setName(name);
-//                    urlInfoBo.setDesc(desc);
-//                    result.add(urlInfoBo);
-//                }
-//            }
-//            if (StringUtils.isNotBlank(this.localFilePath)) {
-//                FileUtil.writeToFile(this.localFilePath, info);// 写入文件
-//            }
-//
-//            System.out.println("链接提取完成！");
-            return result;
+            System.out.println("链接提取完成！");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
-    }
-
-    /**
-     * 获取下载地址
-     *
-     * @param url
-     * @return
-     * @throws Exception
-     */
-    public String getDownloadUrl(String url) throws Exception {
-//        String url = "http://dcdiban.com/" + "view/index5880.html";
-        if (StringUtils.isNoneBlank(url)) {
-            Document doc = Jsoup.parse(new URL(url), 30000);
-            if (doc != null) {
-                Elements a = doc.select(".videourl a");
-                if (a != null && a.size() > 0) {
-                    String videourl = "http://dcdiban.com" + a.attr("href");
-                    Document video = Jsoup.parse(new URL(videourl), 30000);
-//                        System.out.println(video.toString());
-                    if (video != null) {
-                        Elements scripts = video.select("script[src^='/playdata']");
-                        String js = "http://dcdiban.com" + scripts.attr("src");
-                        URL u = new URL(js);
-                        InputStream ism = u.openStream();
-                        BufferedInputStream bf = new BufferedInputStream(ism);
-                        byte[] bytes = new byte[1024];
-                        int len = bf.read(bytes);
-                        StringBuffer sb = new StringBuffer();
-                        while (len != -1) {
-                            len = bf.read(bytes);
-                            String str = new String(bytes, "utf-8");
-                            sb.append(str.trim());
-                        }
-                        String s = sb.toString();
-                        if (StringUtils.isNoneBlank(s)) {
-                            if (s.indexOf("https") != -1 && s.indexOf("$ckplayer") != -1) {
-                                s = s.substring(s.indexOf("https"), s.indexOf("$ckplayer"));
-                                //  "https://201806.53didi.com/20180826/16/1/xml/91_0632500b07744276b46f665e56a4e871.mp4";
-                                return s;
-                            }
-                        }
-                    } else {
-                        System.out.println("没有获取到地址");
-                    }
-                }
-            }
-        }
-
-        return "";
-    }
-
-
-    public String getLocalFilePath() {
-        return localFilePath;
-    }
-
-    public void setLocalFilePath(String localFilePath) {
-        this.localFilePath = localFilePath;
     }
 }
