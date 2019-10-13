39bafb1fb465abe6e5bd5d4abeae48fa1fc55edf
huawei <1170137052@qq.com>
2019-10-14 00:26:09 +0800

    jhfdkj

--- huawei.spider.center.jar/src/main/java/com/huawei/spider/center/utils/AppleScriptUtils.java
+++ huawei.spider.center.jar/src/main/java/com/huawei/spider/center/utils/AppleScriptUtils.java
@@ -2,6 +2,10 @@ package com.huawei.spider.center.utils;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
 
 /**
  * Created by huawei on 2019/10/13.
@@ -9,7 +13,7 @@ import javax.script.ScriptEngineManager;
 public class AppleScriptUtils {
 
     public static void main(String[] args) {
-        testAppleScript();
+        testAppleScriptFromFile();
     }
 
     public static void testAppleScript() {
@@ -18,11 +22,28 @@ public class AppleScriptUtils {
             ScriptEngine engine = manager.getEngineByName("AppleScriptEngine");
             if (engine != null) {
                 StringBuffer buffer = new StringBuffer();
-                buffer.append("tell application \"Terminal\" ");
-                buffer.append("do script echo 'hello' ");
-                buffer.append("end tell");
+                buffer.append("tell application \"Terminal\"\n");
+                buffer.append("do script \"echo 'hello'\"\n");
+                buffer.append("end tell\n ");
                 System.out.println(buffer.toString());
-//                engine.eval(buffer.toString());
+                engine.eval(buffer.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testAppleScriptFromFile() {
+        try {
+            ScriptEngineManager manager = new ScriptEngineManager();
+            ScriptEngine engine = manager.getEngineByName("AppleScriptEngine");
+            if (engine != null) {
+                String filepath = "/mydoc/workspace/applescript/test.scpt";
+                File script = new File(filepath);
+                if(script.exists()) {
+                    Reader reader = new FileReader(script);
+                    engine.eval(reader);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
